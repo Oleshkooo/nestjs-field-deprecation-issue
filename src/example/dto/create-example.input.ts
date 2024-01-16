c2b0bdf68fc561f6cde2be77a6a558745fafc384
Oleshkooo <85068263+Oleshkooo@users.noreply.github.com>
2024-01-16 17:14:31 +0200

    Update create-example.input.ts

--- src/example/dto/create-example.input.ts
+++ src/example/dto/create-example.input.ts
@@ -1,8 +1,7 @@
-import { InputType, Int, Field, Directive } from '@nestjs/graphql';
+import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateExampleInput {
-  @Directive('@deprecated(reason: "This field is deprecated")')
   @Field(() => Int, {
     description: 'Example field (placeholder)',
     deprecationReason: 'This field is deprecated',
